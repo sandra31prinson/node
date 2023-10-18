e385770cc685f08c869ac7d2a3e2e39fd8d0a320
sandra prinson <sandraprinson31@gmail.com>
2023-10-18 17:02:20 +0530

    git ignor file

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// -----------array printing and pushing----
+
+const arra=['sandra', 22 , 'hi']     
+
+
+
+    arra.push('cherpu')
+    console.log(arra);
+
+// --------function with  return --------
+    function myFunction(a,b)
+    {
+        return a*b
+    }
+    console.log(myFunction(5,10))
+
+    // ------arrow function ---
+
+    fun=(a,b)=>{
+        return a*b
+    }
+    console.log(fun(10,30))
+
+    // -----arrow function only have one value---
+
+    arrowFunc=()=>10;
+
+    console.log(arrowFunc());
+
