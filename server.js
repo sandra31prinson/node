5328e31d646228d65f43b5176b001895e8ac4c91
sandra prinson <sandraprinson31@gmail.com>
2023-10-25 18:12:50 +0530

    server created

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-var http = require('http')
+var http=require('http')
+
+// const http = require('http');
+// const path = require('path');
+// const fs = require('fs');
 
 http.createServer(Server).listen(7000)
 
@@ -7,3 +11,7 @@ function Server(req,res){
     res.write('hi.. sandra')
     res.end()
 }
+
+// function server(req,res){
+//     res.end("crossroads")
+//   }
