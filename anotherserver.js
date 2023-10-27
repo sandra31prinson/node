1c01a0ca25a2407a815ebe2955f31f99c2923255
sandra prinson <sandraprinson31@gmail.com>
2023-10-27 10:51:41 +0530

    load html page

--- anotherserver.js
+++ anotherserver.js
@@ -1,7 +1,15 @@
 var http = require('http')
+var fs = require('fs')
 
 http.createServer(function(req,res){
+    
+    fs.readFile('index.html',function(err,data){
 
-    res.write('sandra');
+        res.writeHead(200,{'content-Type':'text/html'})
+    res.write(data);
     res.end()
+
+    })
+
+
 }).listen(8000)
